e2d84c4a49f7343b0102878a46da640fa8804c05
Domas Klimavicius <domas.klimavicius@mif.stud.vu.lt>
2024-05-08 20:15:23 +0300

    Update stage title

--- src/main/java/com/example/lab/HelloApplication.java
+++ src/main/java/com/example/lab/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("student-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
+        stage.setTitle("Lab 4");
         stage.setScene(scene);
         stage.show();
     }
