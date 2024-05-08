c212d896631ac72e537ba22e442088cbc4c21259
Domas Klimavicius <domas.klimavicius@mif.stud.vu.lt>
2024-05-08 17:38:49 +0300

    Start creating StudentViewController

---
+++ src/main/java/com/example/lab/controllers/StudentViewController.java
@@ -0,0 +1,54 @@
+package com.example.lab.controllers;
+
+import com.example.lab.student.Student;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class StudentViewController {
+    @FXML
+    private TextField studentName;
+    @FXML
+    private TextField studentSurname;
+
+    @FXML
+    private TableView<Student> studentTableView = new TableView<>();
+
+    private List<Student> students;
+
+    @FXML
+    private void initialize() {
+        students = new ArrayList<>();
+
+        TableColumn<Student, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Student, String> surnameColumn = new TableColumn<>("Surname");
+        surnameColumn.setCellValueFactory(new PropertyValueFactory<>("surname"));
+
+        studentTableView.getColumns().clear();
+        studentTableView.getColumns().add(nameColumn);
+        studentTableView.getColumns().add(surnameColumn);
+
+        studentTableView.getItems().clear();
+//        for (Student student : students) {
+//            studentTableView.getItems().add(student);
+//        }
+
+    }
+
+    @FXML
+    protected void onAddStudentButtonClick() {
+        Student student = new Student(studentName.getText(), studentSurname.getText());
+
+        studentTableView.getItems().add(student);
+        students.add(student);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,8 @@ module com.example.lab {
 
     opens com.example.lab to javafx.fxml;
     exports com.example.lab;
+    opens com.example.lab.controllers to javafx.fxml;
+    exports com.example.lab.controllers;
+    opens com.example.lab.student to javafx.fxml;
+    exports com.example.lab.student;
 }
